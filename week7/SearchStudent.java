105dc43b30cd913f2f06cddf973848f508c2c98b
rrworkscom <rrworks010@gmail.com>
2026-04-09 15:30:42 +0700

    Searching Methods

---
+++ week7/SearchStudent.java
@@ -0,0 +1,72 @@
+package week7;
+
+public class SearchStudent {
+
+    Students20[] listStd;
+    int idx;
+
+    public SearchStudent(int size) {
+        listStd = new Students20[size];
+    }
+
+    public void add(Students20 std) {
+        if (idx < listStd.length) {
+            listStd[idx] = std;
+            idx++;
+        } else {
+            System.out.println("Data is already full");
+        }
+    }
+
+    public void display() {
+        for (Students20 students : listStd) {
+            students.display();
+            System.out.println("------------------------");
+        }
+    }
+
+    public int findSeqSearch(int search) {
+        int position = -1;
+        for (int i = 0; i < listStd.length; i++) {
+            if (listStd[i].nim == search) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+
+    public int findBinarySearch(int search, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (search == listStd[mid].nim) {
+                return (mid);
+            } else if (listStd[mid].nim < search) {
+                return findBinarySearch(search, left, mid - 1);
+            } else {
+                return findBinarySearch(search, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
+    public void showPosition(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data: " + x + " is found in index- " + pos);
+        } else {
+            System.out.println("Data: " + x + " is not found");
+        }
+    }
+
+    void showData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM \t : " + x);
+            System.out.println("Name \t : " + listStd[pos].name);
+            System.out.println("Age \t : " + listStd[pos].age);
+            System.out.println("GPA \t : " + listStd[pos].gpa);
+        } else {
+            System.out.println("Data " + x + " is not found");
+        }
+    }
+}
